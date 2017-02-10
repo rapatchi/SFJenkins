d61fbfbcc31c84b7f06bd51249affe881c5cb249
Sayantan Saha <saysa@microsoft.com>
2017-02-10 18:35:49 +0530

    temp

--- HelloWorldStateless/HelloWorldStateless/src/statelessservice/HelloWorldStatelessService.java
+++ HelloWorldStateless/HelloWorldStateless/src/statelessservice/HelloWorldStatelessService.java
@@ -14,7 +14,7 @@ public class HelloWorldStatelessService extends StatelessService {
         return super.createServiceInstanceListeners();
     }
 
-/*    @Override
+/*   @Override
     protected CompletableFuture<?> runAsync() {
         // TODO: Replace the following with your own logic.
         return super.runAsync();
