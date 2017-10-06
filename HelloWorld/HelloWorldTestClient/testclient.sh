2852ce532783052359ff625cf0811fabdb9834ec
Sayantan Saha <saysa@microsoft.com>
2017-10-06 13:49:48 +0530

    Removed old actor example

--- HelloWorld/HelloWorldTestClient/testclient.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/out/lib/lib:/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code/
-java -Djava.library.path=$LD_LIBRARY_PATH -jar out/lib/HelloWorld-test.jar
