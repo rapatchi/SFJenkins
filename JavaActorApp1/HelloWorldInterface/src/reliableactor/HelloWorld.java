4d98c654e524625c2245781d68171bb4713c94f8
Sayantan Saha <saysa@microsoft.com>
2017-10-06 14:46:27 +0530

    minor

--- JavaActorApp1/HelloWorldInterface/src/reliableactor/HelloWorld.java
+++ JavaActorApp1/HelloWorldInterface/src/reliableactor/HelloWorld.java
@@ -8,6 +8,5 @@ import microsoft.servicefabric.actors.Readonly;
 public interface HelloWorld extends Actor {
 	@Readonly
 	CompletableFuture<Integer> getCountAsync();
-
 	CompletableFuture<?> setCountAsync(int count);
 }
